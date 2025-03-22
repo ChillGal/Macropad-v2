3e59b0487dd4523a7b9e670c32e28838ecb72dd4
Jennifer Chan <55716891+ChillGal@users.noreply.github.com>
2025-03-22 13:47:41 +0000

    Add basic register calls + Read and write IO

--- MCP23017.c
+++ MCP23017.c
@@ -9,35 +9,78 @@
 #include "hardware/i2c.h"
 #include "MCP23017.h"
 
-uint8_t MCP23017_Initialise(MCP23017 *dev, i2c_inst_t *i2c_instance) {
-    uint8_t i2c_instance = 0;
-    return i2c_instance;
+uint8_t MCP23017_Initialise(MCP23017 *dev, i2c_inst_t *i2c_instance, uint8_t MCP23017_ADDRESS) {
+    
+    // Checks HW I2C is functional + Valid MCP23017 address range
+    if (i2c_instance == NULL || MCP23017_ADDRESS < 0b0100000 || MCP23017_ADDRESS > 0b0100111) {
+        return 1;
+    }
+
+    // Setup struct
+    dev->i2c_instance= i2c_instance;
+    dev->GPIOA[0] = 0;
+    dev->GPIOA[1] = 0;
+    dev->GPIOA[2] = 0;
+    dev->GPIOA[3] = 0;
+
+    dev->GPIOA[4] = 0;
+    dev->GPIOA[5] = 0;
+    dev->GPIOA[6] = 0;
+    dev->GPIOA[7] = 0;
+
+    dev->GPIOB[0] = 0;
+    dev->GPIOB[1] = 0;
+    dev->GPIOB[2] = 0;
+    dev->GPIOB[3] = 0;
+
+    dev->GPIOB[4] = 0;
+    dev->GPIOB[5] = 0;
+    dev->GPIOB[6] = 0;
+    dev->GPIOB[7] = 0;
+    return 0;
 }
 
+// Read all 16 GPIOs at once
 uint8_t MCP23017_ReadIO(MCP23017 *dev) {
-    return 0;
+    uint8_t data[2] = 0;
+    data[0] = MCP23017_ReadRegister(dev, MCP23017_REG_GPIOA); // Bank A
+    data[1] = MCP23017_ReadRegister(dev, MCP23017_REG_GPIOB); // Bank B
+    return data;
 }
 
-uint8_t MCP23017_WriteIO(MCP23017 *dev) {
-    return 0;
+// Writes all 16 IO at once
+void MCP23017_WriteIO(MCP23017 *dev, uint8_t *data) {
+    MCP23017_WriteRegister(dev, MCP23017_REG_GPIOA, &data[0]);
+    MCP23017_WriteRegister(dev, MCP23017_REG_GPIOB, &data[1]);
 }
 
-uint8_t MCP23017_SetIOMODE(MCP23017 *dev) {
-    return 0;
+void MCP23017_SetIOMODE(MCP23017 *dev, uint8_t *mode) {
+
 }
 
-uint8_t MCP23017_SetIOMODE(MCP23017 *dev) {
-    return 0;
+void MCP23017_SetPullups(MCP23017 *dev, uint8_t *pullup) {
+
 }
 
-uint8_t MCP23017_ReadRegister(MCP23017 *dev, uint8_t register, uint8_t *data) {
-    return i2c_read_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, data, 1, false);
+// Reads 1 byte into <data> from the register specified by <reg_address>
+uint8_t MCP23017_ReadRegister(MCP23017 *dev, uint8_t reg_address) {
+    uint8_t data;
+    i2c_write_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, &reg_address,1, true);
+    i2c_read_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, &data, 1, false);
+    return data;
 }
 
-uint8_t MCP23017_ReadRegisters(MCP23017 *dev, uint8_t register, uint8_t *data, uint8_t length) {
-    return i2c_read_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, data, length, false);
+// Maybe not needed?
+uint8_t MCP23017_ReadRegisters(MCP23017 *dev, uint8_t reg_address, uint8_t length) {
+    uint8_t data[length];
+    i2c_write_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, &reg_address, 1, true);
+    i2c_read_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, &data, length, false);
+    return data;
 }
 
-uint8_t MCP23017_WriteRegister(MCP23017 *dev, uint8_t register, uint8_t *data) {
-    return i2c_write_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, data, sizeof(data), false);
+// Writes all of <data> to the register specified by <reg_address>
+void MCP23017_WriteRegister(MCP23017 *dev, uint8_t reg_address, uint8_t *data) {
+    i2c_write_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, reg_address, 1, true);
+    i2c_write_blocking(dev->i2c_instance, MCP23017_I2C_ADDRESS, data, sizeof(data), false);
 }
+
--- MCP23017.h
+++ MCP23017.h
@@ -4,6 +4,7 @@
  *  
  *  Author: Jennifer Chan
  *  Created: 22/03/2025
+ *  Updated: 22/03/2025
  *  Revision: 0.1
  * 
 */
@@ -25,11 +26,11 @@
 #define MCP23017_REG_GPINTENB   0x05
 #define MCP23017_REG_DEFVALA    0x06 // Default Value
 #define MCP23017_REG_DEFVALB    0x07
-#define MCP23017_REG_INTCONA    0x08 // Interrupt on GPIO Change
+#define MCP23017_REG_INTCONA    0x08 // Interrupt on GPIO change
 #define MCP23017_REG_INTCONB    0x09
 #define MCP23017_REG_IOCONA     0x0A // IO expander configuration
 #define MCP23017_REG_IOCONB     0x0B
-#define MCP23017_REG_GPPUA      0x0C // General purpose IO
+#define MCP23017_REG_GPPUA      0x0C // GPIO pullups
 #define MCP23017_REG_GPPUB      0x0D
 #define MCP23017_REG_INTFA      0x0E // Interrupt flag
 #define MCP23017_REG_INTFB      0x0F
@@ -37,7 +38,7 @@
 #define MCP23017_REG_INTCAPB    0x11
 #define MCP23017_REG_GPIOA      0x12 // GPIO values
 #define MCP23017_REG_GPIOB      0x13
-#define MCP23017_REG_OLATA      0x14 // Open Latching
+#define MCP23017_REG_OLATA      0x14 // Output Latching
 #define MCP23017_REG_OLATB      0x15
 
 typedef struct {
@@ -51,15 +52,21 @@ typedef struct {
     
 } MCP23017;
 
-uint8_t MCP23017_Initialise(MCP23017 *dev, i2c_inst_t *i2c_instance);
+uint8_t MCP23017_Initialise(MCP23017 *dev, i2c_inst_t *i2c_instance, uint8_t MCP23017_ADDRESS);
 
 uint8_t MCP23017_ReadIO(MCP23017 *dev);
-uint8_t MCP23017_WriteIO(MCP23017 *dev);
-uint8_t MCP23017_SetIOMODE(MCP23017 *dev);
-uint8_t MCP23017_SetPullups(MCP23017 *dev);
+void MCP23017_WriteIO(MCP23017 *dev, uint8_t *data);
+void MCP23017_SetIOMODE(MCP23017 *dev, uint8_t *mode);
+void MCP23017_SetPullups(MCP23017 *dev, uint8_t *pullup);
 
-// Lower level
-uint8_t MCP23017_ReadRegister(MCP23017 *dev, uint8_t register, uint8_t *data);
-uint8_t MCP23017_ReadRegisters(MCP23017 *dev, uint8_t register, uint8_t *data, uint8_t length);
-uint8_t MCP23017_WriteRegister(MCP23017 *dev, uint8_t register, uint8_t *data);
+// Direct register manipulation
+
+// Read a single byte from specified register <reg_address>
+uint8_t MCP23017_ReadRegister(MCP23017 *dev, uint8_t reg_address); 
+
+// Read <length> bytes from specified register <reg_address>
+uint8_t MCP23017_ReadRegisters(MCP23017 *dev, uint8_t reg_address, uint8_t length);
+
+// Write a single byte of data <data> to specified register <reg_address>
+void MCP23017_WriteRegister(MCP23017 *dev, uint8_t reg_address, uint8_t *data);
 #endif
