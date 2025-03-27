19063327bae065daefe4dd5ac4fb59cccc969f7c
Jennifer Chan <55716891+ChillGal@users.noreply.github.com>
2025-03-27 00:36:40 +0000

    update to start testing on physical HW

--- Macropad.c
+++ Macropad.c
@@ -3,7 +3,7 @@
 #include "hardware/spi.h"
 #include "hardware/i2c.h"
 #include "mcp23017.h"
-
+#include <malloc.h>
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -17,14 +17,14 @@
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SDA 6
+#define I2C_SCL 7
 
 
 
 int main()
 {
-    // stdio_init_all();
+    stdio_init_all();
 
     // // SPI initialisation. This example will use SPI at 1MHz.
     // spi_init(SPI_PORT, 1000*1000);
@@ -57,18 +57,37 @@ int main()
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
     MCP23017 mcp;
+    uint8_t *ptr = 0;
     uint8_t result = MCP23017_Initialise(&mcp, I2C_PORT, 0x20);
-    int sleep_time = 100; // 100ms default
-    if (result == 0) {
+    uint8_t *dirptr = malloc(sizeof(uint8_t) * 2);
+    dirptr[0] = 255;
+    dirptr[1] = 255;
+    MCP23017_SetIODirection(&mcp,dirptr);
+
+    uint8_t *pullupptr = malloc(sizeof(uint8_t) * 2);
+    pullupptr[0] = 255;
+    pullupptr[1] = 255;
+    MCP23017_SetPullups(&mcp,pullupptr);
+    ptr = MCP23017_GetIO(&mcp);
+    printf("%d", result);
+    int sleep_time = 200; // 10ms default
+    if (result == 1) {
         sleep_time = 1000;
     }
     const uint LED_PIN = 25; // LED pin is fixed at 25
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(sleep_time);
+    if (&ptr[0] == 0){
         gpio_put(LED_PIN, 1);
-        sleep_ms(sleep_time);
     }
+    else {
+        gpio_put(LED_PIN, 0);
+    }
+
+    // while (true) {
+    //     gpio_put(LED_PIN, 0);
+    //     sleep_ms(sleep_time);
+    //     gpio_put(LED_PIN, 1);
+    //     sleep_ms(sleep_time);
+    // }
 }
