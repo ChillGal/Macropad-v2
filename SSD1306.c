0a2ecd23a8282a694ebc4ea9665b7174c8945e1f
Jennifer Chan <55716891+ChillGal@users.noreply.github.com>
2025-04-12 17:24:32 +0100

    Add basic read and write registers based from MCP23017 implementation + include a test function for display

--- SSD1306.c
+++ SSD1306.c
@@ -19,7 +19,7 @@
 
 uint8_t SSD1306_Initialise(SSD1306 *dev, i2c_inst_t *i2c_instance, uint8_t ssd1306_address, uint8_t ssd1306_height, uint8_t ssd1306_width) {
     // Checks HW I2C is functional + Valid SSD1306 address range
-    if (i2c_instance == NULL || ssd1306_address < 0b00111100 || ssd1306_address > 0b00111101) { // 0x20 to 0x27
+    if (i2c_instance == NULL || ssd1306_address < 0b00111100 || ssd1306_address > 0b00111101) { // 0x3c to 0x3d
         return 1;
     }
 
@@ -28,5 +28,36 @@ uint8_t SSD1306_Initialise(SSD1306 *dev, i2c_inst_t *i2c_instance, uint8_t ssd13
     dev->ssd1306_i2c_addr = ssd1306_address;
     dev->height = ssd1306_height;
     dev->width = ssd1306_width;
+
     return 0;
+}
+
+void SSD1306_DisplayPowerOn(SSD1306 *dev) {
+    uint8_t *data = malloc(sizeof(uint8_t));
+    *data = SSD1306_POWERON;
+    SSD1306_WriteRegister(dev, 0x00, data);
+    free(data);
+}
+
+
+// Reads 1 byte into <data> from the register specified by <reg_address>
+uint8_t SSD1306_ReadRegister(SSD1306 *dev, uint8_t reg_address) {
+    uint8_t *data = malloc(sizeof(uint8_t));
+    uint8_t *reg = malloc(sizeof(uint8_t));
+    *reg = reg_address;
+    i2c_write_blocking(dev->i2c_instance, dev->ssd1306_i2c_addr, reg, 1, true);
+    free(reg);
+    i2c_read_blocking(dev->i2c_instance, dev->ssd1306_i2c_addr, data, 1, false);
+    uint8_t out = *data;
+    free(data);
+    return out;
+}
+
+// Writes all of <data> to the register specified by <reg_address>
+void SSD1306_WriteRegister(SSD1306 *dev, uint8_t reg_address, uint8_t *data) {
+    uint8_t *reg = malloc(sizeof(uint8_t));
+    *reg = reg_address;
+    i2c_write_blocking(dev->i2c_instance, dev->ssd1306_i2c_addr, reg, 1, true);
+    free(reg);
+    i2c_write_blocking(dev->i2c_instance, dev->ssd1306_i2c_addr, data, sizeof(*data), false);
 }
--- SSD1306.h
+++ SSD1306.h
@@ -14,7 +14,10 @@
 #define _SSD1306_H
 
 // I2C address
-#define SSD1306_I2C_ADDRESS    0x3c // Default address is 0x20. Range from 0x20-0x27, bit ordering is 0 0 1 0 0 A2 A1 A0
+#define SSD1306_I2C_ADDRESS    0x3C // Default address is 0x20. Range from 0x20-0x27, bit ordering is 0 0 1 0 0 A2 A1 A0
+
+#define SSD1306_POWERON         0xAE
+#define SSD1306_POWEROFF        0xAF
 
 typedef struct {
     
@@ -26,7 +29,10 @@ typedef struct {
 
 } SSD1306;
 
-uint8_t SSD1306_Initialise(SSD1306 *dev, i2c_inst_t *i2c_instance, uint8_t ssd1306_address);
+uint8_t SSD1306_Initialise(SSD1306 *dev, i2c_inst_t *i2c_instance, uint8_t ssd1306_address, uint8_t ssd1306_height, uint8_t ssd1306_width);
 
+void SSD1306_DisplayPowerOn(SSD1306 *dev);
 
+uint8_t SSD1306_ReadRegister(SSD1306 *dev, uint8_t reg_address);
+void SSD1306_WriteRegister(SSD1306 *dev, uint8_t reg_address, uint8_t *data);
 #endif
